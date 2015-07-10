c4cfbab2bd1b8c77e9c16c6783bae608d1030d3c
Hackbright Student <info@hackbrightacademy.com>
2015-07-10 13:02:36 -0700

    before lunch save again

---
+++ tweet.py
@@ -0,0 +1,13 @@
+import os
+import twitter
+
+api = twitter.Api(
+    consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+print api.VerifyCredentials()
+
+status = api.PostUpdate("Hello world!")
+print status.text
